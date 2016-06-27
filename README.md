23a92f9c36636ac70bd2c1ed3779482d3849ce9f
McZlatan Bourne <bpornethesonofthegun@gmail.com>
2016-06-27 10:52:40 +0100

    first rollout

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # getico_web
+gets all images on a webpage
+
+* supports only homepages for now
+* doesn't get images recursively on the website
+
+
+
---
+++ getico.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from flask import Flask
+from flask import *
+import urllib2
+import re
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        webpage = request.form["webpage_url"]
+        page_type = request.form["page_type"]
+        img_format = request.form["img_format"]
+        try:
+            if webpage.find("http://") == -1:
+                error = "Invalid url. Url should contain \"http://\" or \"https://\""
+                return render_template("getico.html", error=error)
+            elif webpage.find("http://") == 0:
+                try:
+                    if page_type == "other_page":
+                        error = "Supports only home pages for now"
+                        return render_template("getico.html", error=error)
+                    elif page_type == "home_page":
+                        headers = {'User-Agent': 'Mozilla/5.0'}
+                        src = urllib2.urlopen(urllib2.Request(webpage, None, headers)).read()
+                        pat = re.compile ('<img [^>]*src="([^"]+)')
+                        img = pat.findall(src)
+                        return render_template("result.html", img=img, webpage=webpage)
+                    else:
+                        error = "What???"
+                        return render_template("getico.html", error=str(error))
+                except Exception as error:
+                    return render_template("getico.html", error=str(error))
+            else:
+                error = "What???"
+                return render_template("getico.html", error=str(error))
+        except Exception as error:
+            return render_template("getico.html", error=str(error))
+    else:
+        return render_template("getico.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
